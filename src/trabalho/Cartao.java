4fe3b20a2d88416ac7e1c168d763ddab1ab5c4e8
Albrecht <albrechtgoecking@gmail.com>
2020-06-24 00:34:58 -0300

    Criação do projeto

---
+++ src/trabalho/Cartao.java
@@ -0,0 +1,7 @@
+package trabalho;
+
+public class Cartao extends Pagamento {
+	public void Pagar() {
+		System.out.println("Pagamento em Cartao");
+	}
+}
---
+++ src/trabalho/Dinheiro.java
@@ -0,0 +1,8 @@
+package trabalho;
+
+public class Dinheiro extends Pagamento{
+	@Override
+	public void Pagar() {
+		System.out.println("Pago em dinheiro");
+	}
+}
