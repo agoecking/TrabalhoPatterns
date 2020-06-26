3ba2e9193ee3dd4148d426886d11e05d8b128561
Albrecht <albrechtgoecking@gmail.com>
2020-06-26 20:55:02 -0300

    Versao final de verdade

--- src/trabalho/TestarLoja.java
+++ src/trabalho/TestarLoja.java
@@ -11,29 +11,48 @@
 //que, com uso de polimorfismo, estrutura os estados da máquina. Essa por sua vez, é 
 //construída em baixo acoplamento, evitando que um objeto tenha grande interferencia em outro.
 
-
 package trabalho;
 
 public class TestarLoja {
 
 	public static void main(String[] args) {
-		
+
 		ControleLoja controle = new ControleLoja(new PedidoAberto());
+
+		// teste 1
+
+		controle.Pagar();
+
+		controle.Cancelar();
 		
-		//teste 1
+		System.out.println();
+
+		// teste 2
+
+		controle.setEstadoCompra(new PedidoAberto());
+
+		controle.Cancelar();
+
+		controle.Pagar();
 		
+		System.out.println();
+
+		// teste 3
+
+		controle.setEstadoCompra(new PedidoCancelado());
+
 		controle.Pagar();
 		
 		controle.Cancelar();
 		
-		controle.setEstadoCompra(new PedidoAberto());
+		System.out.println();
 		
-		//---
+		// teste 4
 		
-		//teste 2
-		
+		controle.setEstadoCompra(new PedidoPago());
+
 		controle.Cancelar();
-		
+
 		controle.Pagar();
 	}
 
